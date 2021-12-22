8d4af0590e44fef59b48a6d73561a15959d73566
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-22 22:35:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     readme_content_decoded = decode_readme(readme_repo.get_readme().content)
     new_readme = generate_new_readme(readme=readme_content_decoded, image_tag=image_tag)
     if readme_content_decoded != new_readme:
-        readme_repo.update_file(path=readme_obj.path, message=COMMIT_MSG, content=new_readme, sha=readme_obj.sha)
+        readme_repo.update_file(path=readme_repo.get_readme().path, message=COMMIT_MSG, content=new_readme, sha=readme_repo.get_readme().sha)
         print("Success")
     else:
         print("No change")
